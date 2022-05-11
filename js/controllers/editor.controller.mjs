070d2fa7bdc9cec35b0e8b05a83c0e3bd8d8c0ae
naeun.chae <naeun.chae@bespinglobal.com>
2022-05-11 13:39:33 +0900

    - view, controller, service 간의 데이터 주고받기 기능 개발 - 로컬스토리지, model에 데이터 저장 및 렌더링 완료 - mocha 테스트 프레임워크 설치 및 구조 설계

--- js/controllers/editor.controller.mjs
+++ js/controllers/editor.controller.mjs
@@ -6,6 +6,20 @@ export class EditorController {
 
     this.editorView.btnsEdit();
 
+    this.editorView.inputText(this.setText);
+    this.editorView.refreshText(this.getText());
+
   }
+
+  // 실시간 문자 받아서 모델, 로컬스토리지에 저장하기
+  setText = text => {
+    this.editorService.addTextToModel(text);
+  }
+
+  // 로컬스토리지에 저장되어있는 값 가져오기
+  getText() {
+    return this.editorService.getStorageText();
+  }
+
 
 }
--- js/services/editor.service.mjs
+++ js/services/editor.service.mjs
@@ -1,6 +1,27 @@
+import {DocumentModel} from "../models/document.model.mjs";
+
 export class EditorService {
   constructor() {
-    const text = JSON.parse(localStorage.getItem('text'));
+    const ts = JSON.parse(localStorage.getItem('text')) || [];
+    this.model = new DocumentModel(ts);
+
+  }
+
+  // 로컬스토리지에 저장
+  _commit(model) {
+    localStorage.setItem("text", JSON.stringify(model));
+
+  }
+
+  // 모델에 저장
+  addTextToModel(ts) {
+    this.model = ts;
+
+    this._commit(this.model);
+  }
+
+  getStorageText() {
+    return JSON.parse(localStorage.getItem('text'));
   }
 
 }
